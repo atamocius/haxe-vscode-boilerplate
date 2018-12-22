9666858792c1ce193f33a3748aed9ab23d29318c
Daniel Anton Mata <anton.mata@gmail.com>
2018-12-22 19:55:35 +0800

    Minor modification for consistency

--- src/Main.hx
+++ src/Main.hx
@@ -1,7 +1,7 @@
 import src.FizzBuzz;
 
 class Main {
-  static public function main(): Void {
+  public static function main(): Void {
     final result = FizzBuzz.fizzBuzz(1, 100);
     for (i in result) {
       trace(i);
